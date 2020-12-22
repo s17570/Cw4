92973ab3b73b988bc00eb9e797eb2caab579ff11
s17570 <s17570@pjwstk.edu.pl>
2020-12-22 12:59:47 +0100

    Ukonczenie zad 4.2

---
+++ Cw4/Cw4/Controllers/StudentsController.cs
@@ -0,0 +1,57 @@
+﻿using Cw4.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cw4.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            var list = new List<Student>();
+
+            using (SqlConnection con = new SqlConnection(getConnectionString("db-mssql", "s17570", true)))
+            using (SqlCommand com = new SqlCommand())
+            {
+                com.Connection = con;
+                com.CommandText = "SELECT student.firstname, student.lastname, student.birthdate, studies.name, enrollment.semester " +
+                    "FROM student " +
+                    "INNER JOIN enrollment ON student.idenrollment=enrollment.idenrollment " +
+                    "INNER JOIN studies ON enrollment.idstudy=studies.idstudy";
+
+                con.Open();
+                SqlDataReader dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+                    var st = new Student();
+                    st.FirstName = dr["FirstName"].ToString();
+                    st.LastName = dr["LastName"].ToString();
+                    st.BirthDate = dr["BirthDate"].ToString();
+                    st.StudiesName = dr["Name"].ToString();
+                    st.Semester = dr["Semester"].ToString();
+                    list.Add(st);
+                }
+            }
+
+            return Ok(list);
+        }
+        
+
+        public string getConnectionString(string dataSource, string initialCatalog, bool isIntegratedSecurity)
+        {
+            var connectionBuilder = new SqlConnectionStringBuilder();
+            connectionBuilder.DataSource = dataSource;
+            connectionBuilder.InitialCatalog = initialCatalog;
+            connectionBuilder.IntegratedSecurity = isIntegratedSecurity;
+
+            return connectionBuilder.ConnectionString;
+        }
+    }
+}
